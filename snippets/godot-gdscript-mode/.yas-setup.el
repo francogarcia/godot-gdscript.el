ee58f50f393439b8005599459626c3c703dffce3
Franco Eusébio Garcia <francoegarcia@outlook.com>
2016-02-28 10:41:27 -0300

    Enable snippet expansion in comments and strings.
    In the future, the code might be un-commented to disable the expansion
    in such cases.

--- snippets/godot-gdscript-mode/.yas-setup.el
+++ snippets/godot-gdscript-mode/.yas-setup.el
@@ -1,2 +1,6 @@
-# (add-hook godot-gdscript-mode-hook
-#           '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
+;; (add-hook godot-gdscript-mode-hook
+;;           '(lambda ()
+;;              ;;(set (make-local-variable 'yas-indent-line) 'fixed)
+;;              (set (make-local-variable
+;;                    'buffer-local-condition)
+;;                   ((not (godot-gdscript-syntax-comment-or-string-p))))))
