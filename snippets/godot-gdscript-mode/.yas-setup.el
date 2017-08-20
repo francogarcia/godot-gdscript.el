d82b01231cce527e32bf1fb18749ee0f29d2e2de
Franco Eusébio Garcia <francoegarcia@outlook.com>
2017-08-20 12:24:43 -0300

    Moved auto-completion and snippets to their own repositories.
    - https://github.com/francogarcia/company-godot-gdscript.el
    - https://github.com/francogarcia/yasnippet-godot-gdscript

--- snippets/godot-gdscript-mode/.yas-setup.el
+++
@@ -1,6 +0,0 @@
-;; (add-hook godot-gdscript-mode-hook
-;;           '(lambda ()
-;;              ;;(set (make-local-variable 'yas-indent-line) 'fixed)
-;;              (set (make-local-variable
-;;                    'buffer-local-condition)
-;;                   ((not (godot-gdscript-syntax-comment-or-string-p))))))
